23d3047bcb989e5111c0573ddd16b9d9ecb1edd9
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2020-11-04 08:57:06 +0100

    feat: home page and nav

---
+++ src/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <Nav />
+</template>
+
+<script>
+import Nav from './components/Nav.vue'
+    export default {
+        name: 'Home',
+        components: {
+            Nav,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/home/components/Nav.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="sm">
+      <div class="logo">
+        <div class="logoBox">
+          <img src="./../../assets/eloquent-logo.png" alt="eloquent logo" />
+        </div>
+      </div>
+    </div>
+    <div class="nav">
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Our Service</a>
+        </li>
+        <li>
+          <a href="#">Our Team</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Nav",
+};
+</script>
+
+<style scoped>
+</style>

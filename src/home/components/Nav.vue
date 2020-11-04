12cfd8bd932e105699d2997eaa011fd282f2da0b
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2020-11-04 09:21:04 +0100

    style: toggle bar

--- src/home/components/Nav.vue
+++ src/home/components/Nav.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>
+  <div class="header">
     <div class="sm">
       <div class="logo">
         <div class="logoBox">
           <img src="./../../assets/eloquent-logo.png" alt="eloquent logo" />
+        </div>
+      </div>
+      <!--  -->
+      <div class="toggleBtn">
+        <div class="toggleBarHolder">
+          <div class="toggleBarA"></div>
+          <div class="toggleBarB"></div>
+          <div class="toggleBarC"></div>
         </div>
       </div>
     </div>
@@ -33,4 +41,50 @@ export default {
 </script>
 
 <style scoped>
+.header {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.sm {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.logo {
+  height: auto;
+  background: green;
+}
+.logoBox {
+  height: 50px;
+  object-fit: contain;
+}
+.logoBox img {
+  height: 50px;
+  object-fit: contain;
+}
+/*  */
+.toggleBarHolder {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
+.toggleBarA {
+  width: 30px;
+}
+.toggleBarB {
+  width: 20px;
+}
+.toggleBarC {
+  width: 10px;
+}
+.toggleBarA,
+.toggleBarB,
+.toggleBarC {
+  background: black;
+  height: 2px;
+  margin: 3px 0px;
+  transition: 0.3s all;
+}
 </style>
